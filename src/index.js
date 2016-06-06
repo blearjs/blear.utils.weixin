3c10df7d58f2a7a37d4718a2abd9590aedc3ead3
#云淡然 <cloudcome@163.com>
2016-06-06 19:46:15 +0800

    complete

--- src/index.js
+++ src/index.js
@@ -211,7 +211,7 @@ var Weixin = Class.extend({
 
         wx.getLocation({
             type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-            success: the[_callbackWrapper](callback)
+            complete: the[_callbackWrapper](callback)
         });
 
         return the;
