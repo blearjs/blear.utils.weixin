5606aa2085d4ba5d0687821aa65da31b78e198f2
#云淡然 <cloudcome@163.com>
2017-10-23 16:14:44 +0800

    ~ 打开地图默认 28 级别

--- src/index.js
+++ src/index.js
@@ -269,12 +269,12 @@ var Weixin = Class.extend({
     /**
      * 使用微信内置地图打开位置，可以打开第三方地图
      * @param config
-     * @param config.longitude
-     * @param config.latitude
-     * @param config.name
-     * @param config.address
-     * @param config.scale
-     * @param config.infoUrl
+     * @param config.longitude 经度，浮点数，范围为180 ~ -180
+     * @param config.latitude 纬度，浮点数，范围为90 ~ -90
+     * @param config.name 位置名
+     * @param config.address 地址详情说明
+     * @param config.scale 地图缩放级别,整形值,范围从1~28。默认为最大
+     * @param config.infoUrl 在查看位置界面底部显示的超链接,可点击跳转
      */
     openLocation: function (config) {
         wx.openLocation(object.assign({}, {
@@ -282,7 +282,7 @@ var Weixin = Class.extend({
             latitude: 0,
             name: '',
             address: '',
-            scale: 1,
+            scale: 28,
             infoUrl: ''
         }, config));
     },
